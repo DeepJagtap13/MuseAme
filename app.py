ae005d557d51dc4d240533444628542a54f86e0a
Deep Jagtap <co2022.deep.jagtap@ves.ac.in>
2025-03-07 18:28:14 +0530

    Fixed port issue

--- app.py
+++ app.py
@@ -96,5 +96,8 @@ def ask():
     session.modified = True
     return jsonify({'response': response})
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # Default to 10000
+    app.run(host='0.0.0.0', port=port)
